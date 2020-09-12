f2077a4bad73b5f2a772924173d7688dfdbdec7d
kidstylex <chilizhiki@gmail.com>
2020-09-12 16:02:44 +0700

    modified mysite/urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+import polls.views as views
 
 urlpatterns = [
+    path('',views.IndexView.as_view()),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
 ]
