c55b1f1d83153d3bf55615f85ed554e7d1d6aeb2
kidstylex <chilizhiki@gmail.com>
2020-09-18 13:25:47 +0700

    add is_published and can_vote methods

--- polls/models.py
+++ polls/models.py
@@ -13,10 +13,19 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        now = timezone.now()
+        return now >= self.pub_date
+
+    def can_vote(self):
+        now = timezone.now()
+        return self.end_date > now >= self.pub_date
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
-    
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
