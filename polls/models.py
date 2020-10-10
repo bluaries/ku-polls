3580d0f9c62214822a8ee6719dd53e8bdf6b8cde
kidstylex <chilizhiki@gmail.com>
2020-10-10 18:12:15 +0700

    convert code to be flake8 compliant

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,10 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
+    """Create a question model in polls app."""
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     end_date = models.DateTimeField('ending date', default=None, null=True)
@@ -11,14 +14,20 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
+        """Return True when a question was published recently."""
+
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def is_published(self):
+        """Return True when a question is published."""
+
         now = timezone.now()
         return now >= self.pub_date
 
     def can_vote(self):
+        """Return True when voting allowed"""
+
         now = timezone.now()
         return self.end_date > now >= self.pub_date
 
@@ -28,6 +37,8 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    """Create a choice model in polls app."""
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
--- polls/views.py
+++ polls/views.py
@@ -10,14 +10,20 @@ from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
+    """Show a list of available polls question in index."""
+
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
-    
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()).order_by('-pub_date')
+
+
 class DetailView(generic.DetailView):
+    """Show detail of available polls question."""
+
     model = Question
     template_name = 'polls/detail.html'
 
@@ -25,17 +31,22 @@ class DetailView(generic.DetailView):
         """ Excludes any questions that aren't published yet."""
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+
 class ResultsView(generic.DetailView):
+    """Show results of each polls question."""
+
     model = Question
     template_name = 'polls/results.html'
 
+
 def vote(request, question_id):
+    """Count vote in polls app."""
     question = get_object_or_404(Question, pk=question_id)
 
     try:
-        select_choice =  question.choice_set.get(pk=request.POST["choice"])
+        select_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html',{
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
@@ -43,12 +54,16 @@ def vote(request, question_id):
         select_choice.votes += 1
         select_choice.save()
 
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-        
+        return HttpResponseRedirect(reverse(
+            'polls:results',
+            args=(question.id,)))
+
+
 def vote_allowed(request, pk):
-    question = Question.objects.get(pk = pk)
+    """Check voting allowed for a question."""
+    question = Question.objects.get(pk=pk)
     if not question.can_vote():
-        messages.error(request, f"Voting is not allowed")
+        messages.error(request, f'{"Voting is not allowed"}')
         return redirect('polls:index')
     messages.success(request, "Your choice successfully recorded. Thank you.")
     return render(request, "polls/detail.html", {"question": question})
