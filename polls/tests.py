4acb2e0706e4d52f0d8ad1849797473f3bd6d0fa
kidstylex <chilizhiki@gmail.com>
2020-10-10 22:27:23 +0700

    try to fix error

--- polls/tests.py
+++ polls/tests.py
@@ -17,15 +17,12 @@ def create_question(question_text, days):
 
 
 class QuestionDetailViewTests(TestCase):
-    def test_not_found_question(self):
+    def test_future_question(self):
         """
-        The detail view of a question with a pub_date
-        in the future returns a 404 not found.
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
         """
-
-        future_question = create_question(
-            question_text='Future question.',
-            days=5)
+        future_question = create_question(question_text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
@@ -35,10 +32,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the past
         displays the question's text.
         """
-
-        past_question = create_question(
-            question_text='Past Question.',
-            days=-5)
+        past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
